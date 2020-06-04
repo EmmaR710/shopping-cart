94c8590d5b8e48128d9fac8635c4d730cb07c1ee
Emma Swift <emma@tintinet.co.uk>
2020-06-04 14:19:06 +0100

    Home, Cart and Navbar components created, unnecessary files removed

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Home extends Component{
+
+    render(){
+
+        return(
+            <div className="container">
+                <h3>Add items to your cart here</h3>
+            </div>
+        )
+    }
+
+}
+
+
+export default Home
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap'
+
+const BootstrapNavbar = ()=>{
+    return(
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand>CDL Store</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                    <Nav.Link><Link to="/cart">My cart</Link></Nav.Link>
+                </Nav>
+        </Navbar>
+
+
+    )
+}
+
+export default BootstrapNavbar;
+
